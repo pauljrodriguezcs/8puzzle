373fac867f9af80e43f83cb7ed7b11d5234c3bf6
Paul Rodriguez <prodr010@ucr.edu>
2017-10-25 12:45:24 -0700

    child node created succesfully

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,6 +1,11 @@
 import sys
 import numpy
+import copy
 
+#what the matrix should end up looking like
+goal = numpy.matrix('1 2 3; 4 5 6; 7 8 0')
+
+#custom class for nodes
 class node:
     def __init__(self,state,cost,action=None,parent=None):
         self.state = state
@@ -8,10 +13,11 @@ class node:
         self.action = action
         self.cost = cost
 
+#return new matrix based off of parent and perform action
 def result(parent,action):
+    temp_state = copy.deepcopy(parent)
     
-    temp_state = parent.state
-    
+    #move the blank spot left and return the state
     if action == "left":
         for i in range(numpy.size(temp_state,0)):
             for j in range(numpy.size(temp_state,1)):
@@ -21,6 +27,7 @@ def result(parent,action):
                     temp_state[i][j] = temp
                     return temp_state
 
+    #move the blank spot right and return the state
     elif action == "right":
         for i in range(numpy.size(temp_state,0)):
             for j in range(numpy.size(temp_state,1)):
@@ -30,6 +37,7 @@ def result(parent,action):
                     temp_state[i][j] = temp
                     return temp_state
 
+    #move the blank spot up and return the state
     elif action == "up":
         for i in range(numpy.size(temp_state,0)):
             for j in range(numpy.size(temp_state,1)):
@@ -39,6 +47,7 @@ def result(parent,action):
                     temp_state[i][j] = temp
                     return temp_state
 
+    #move the blank spot down and return the state
     elif action == "down":
         for i in range(numpy.size(temp_state,0)):
             for j in range(numpy.size(temp_state,1)):
@@ -48,18 +57,17 @@ def result(parent,action):
                     temp_state[i][j] = temp
                     return temp_state
 
+#create the child node based off of parent and the action
+def child_node(parent,action):
+    c_state = result(parent.state,action)
+    cost = parent.cost+1
 
-def child_node(parent_node,action):
-    c_state = result(parent_node,action)
-    cost = parent_node.cost+1
-    
-    print(c_state)
-    print(parent_node.state)
-
-    return node(c_state,cost,action,parent_node)
+    return node(c_state,cost,action,parent.state)
 
 
-#def uniform_cost_search(m):
+
+def uniform_cost_search(problem):
+
 
 
 
@@ -67,17 +75,18 @@ def child_node(parent_node,action):
 
 
 def main():
+    
+    #default puzzle
     row1 = [1,2,3]
     row2 = [4,5,6]
     row3 = [7,0,8]
-    
-    goal = numpy.matrix('1 2 3; 4 5 6; 7 8 0')
     
     error = 1
     
     print("Welcome to Paul's 8-puzzle solver")
     print( "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle.")
     
+    #get only valid inputs from user 1 or 2
     while (error == 1):
         puzzleChoice = input()
         
@@ -90,7 +99,8 @@ def main():
         else:
             print("Error: only 1 or 2 allowed")
             error = 1
-    
+
+    #custom puzzle chosen, parse in
     if(puzzleChoice == 2):
         print("Enter your puzzle, use a zero to represent the blank")
         print("Enter the first row, use spaces or tabs between numbers \t")
@@ -106,11 +116,20 @@ def main():
         row3 = raw_input().split()
 
 
+    #choose heuristic to solve problem
+    #
+    #
+    #       FILL ME !!!!!!!!!
+    #
+    #
+
+    #create matrix template
     a = numpy.zeros(shape=(3,3))
 
     i = 0
     j = 0
 
+    #fill in matrix with default or custom values
     for i in range(3):
         for j in range(3):
             if(i == 0):
@@ -122,12 +141,6 @@ def main():
             else:
                 a[i,j] = int(row3[j])
 
-    print(a)
-
-    n = node(a,0,None,None)
-    print(n.state,n.cost,n.action,n.parent)
-
-    m = child_node(n,"left")
-    print(m.state,m.cost,m.action,m.parent.state)
+    value = uniform_cost_search(a)
 
 main()
